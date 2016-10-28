e8480456f867ef57fd6270dd3eb898fb0d608448
Alexander Klimetschek <aklimets@adobe.com>
2016-10-27 19:02:12 -0700

    adding wrk2 formula

---
+++ wrk2.rb
@@ -0,0 +1,19 @@
+class Wrk2 < Formula
+  desc "HTTP benchmarking tool (fork by Gil Tene)"
+  homepage "https://github.com/giltene/wrk2"
+  head "https://github.com/giltene/wrk2.git"
+
+  depends_on "openssl"
+
+  def install
+    ENV.j1
+    system "make"
+    bin.install "wrk"
+    # Prevent a conflict between this and original wrk
+    mv bin/"wrk", bin/"wrk2"
+  end
+
+  test do
+    system "#{bin}/wrk2", "-c", "1", "-t", "1", "-d", "1", "https://example.com/"
+  end
+end
